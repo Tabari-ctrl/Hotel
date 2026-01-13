39c7df615c1bef65af35e0b81e977ca308779a09
Alamin <alamintabari@gmail.com>
2026-01-13 07:59:21 +0100

    addedTheMdImage

--- JS/About.js
+++ JS/About.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = [
-    "./Asset/images/founder.png"
+    "./Asset/images/founder.png",
+    "./Asset/images/MD.jpg"
   ];
 
   let current = 0;
